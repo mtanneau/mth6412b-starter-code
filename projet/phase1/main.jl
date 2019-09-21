96ba2302fe6a1089f30b14d408f70c6e7683a3ce
aureliebcx <aureliebcx@gmail.com>
2019-09-21 18:26:55 -0400

    Test sur la création d'un graph

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -9,11 +9,35 @@ function main()
     noeud2 = Node("Kirk", "guitar")
     noeud3 = Node("Lars", 2)
 
-    edge1 = Edge("1", noeud1, noeud2, 3);
-    edge2 = Edge("2", noeud2, noeud3, 4);
-    read_stsp(joinpath(@__DIR__,"..","..","instances","stsp","bayg29.tsp"))
-
+    edge1 = Edge(noeud1, noeud2, 3)
+    edge2 = Edge(noeud2, noeud3, 4)
+    graphToConstruct = read_stsp(joinpath(@__DIR__,"..","..","instances","stsp","bayg29.tsp"))
+    println(typeof(graphToConstruct))
+    println(graphToConstruct[2])
+    """construct_graph(graphToConstruct[1])"""
 
 end
 
-main();
+
+"""fonction qui va créer un tableau de type noeuds"""
+function construct_graph(nodes_dic::Dict)
+    """création du graphe vide """
+    t = eltype(collect(nodes));
+    nodesgraph = Vector{Node{t}}();
+    edgesgraph = Vector{Edge{t}}();
+    println("t ", typeof(t));
+    println(typeof(nodesgraph))
+    println(edgesgraph)
+    Graph_stsp = Graph("stsp",nodesgraph ,edgesgraph)
+
+    """Pour tous les noeuds du graphe, on crée un objet de type noeud et on l'ajoute au tableau"""
+    for k = 1 : length(nodes)
+        println(nodes[k])
+        node = Node(nodes[k][1],nodes[k][2])
+        add_node!(Graph_stsp,node)
+    end
+    return Graph_stsp
+
+end
+
+main()
