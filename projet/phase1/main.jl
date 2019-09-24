81ac7efc66b6c867263068af206604d393463e4b
Arthur MARIE <arthur.marie@polymtl.ca>
2019-09-23 22:47:33 -0400

    Changed docstring in main

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -4,11 +4,6 @@ using Plots
 """ Analyse une instance de TSP symétrique dont les poids sont donnés au format
  EXPLICIT et construit un objet de type Graph.
 
- La méthode renvoie aussi un tableau répertoriant les poids des arcs au départ de chaque noeud.
- --> graph_edges_weights[i][j] est donc le poids de l'arc reliant le noeud i au noeud j.
-
- Pour finir, la méthode main génère un fichier pdf contenant le graphique produit.
-
 """
 
 function main(filename::String)
