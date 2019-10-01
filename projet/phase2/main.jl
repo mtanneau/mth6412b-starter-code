69137aea6238574ab4b2c38748e1c6d0f586fc22
aureliebcx <aureliebcx@gmail.com>
2019-10-01 19:01:19 -0400

    Tests sur plusieurs instances STSP

--- projet/phase2/main.jl
+++ projet/phase2/main.jl
@@ -11,10 +11,28 @@ include(joinpath(@__DIR__, "graphConstruction.jl"))
 function main()
     file_name = "gr21.tsp"
     #on construit le graphe à partir d'une instance TSP
-    graphe = construct_graph(file_name, "test")
+    graphe1 = construct_graph(file_name, "test")
 
     #on construit l'arbre de recouvrement minimal grâce à l'algo de Kruskal
-    arbre = algoKruskal(graphe)
+    arbre1 = algoKruskal(graphe1)
+    println(arbre1.link)
+
+    #test 2
+    graphe2 = construct_graph("bayg29.tsp", "test2")
+    arbre2 = algoKruskal(graphe2)
+    println(arbre2.link)
+
+    #test3
+    graphe3 = construct_graph("hk48.tsp", "test3")
+    arbre3 = algoKruskal(graphe3)
+    println(arbre3.link)
+
+    #test4
+    graphe4 = construct_graph("swiss42.tsp", "test4")
+    arbre4 = algoKruskal(graphe4)
+    println(arbre4.link)
+
+
 
 end
 
