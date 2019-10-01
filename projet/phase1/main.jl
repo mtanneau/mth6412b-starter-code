9c8fefa338d9a9657580ac5a11e81079fa5cd78e
Florence754 <55095872+Florence754@users.noreply.github.com>
2019-10-01 15:57:33 -0400

    Update main.jl

--- projet/phase1/main.jl
+++ projet/phase1/main.jl
@@ -10,11 +10,11 @@ graph_edges = read_edges(header,filename)
 graph_nodes = read_nodes(header,filename)
 G = Graph(nom, Node{Vector{Float64}}[], Edge{Vector{Float64}}[])
 # On ajoute les noueds un par un au graphe G
-for i in keys(graph_nodes)
-    add_node!(G, Node(string(i), graph_nodes[i]))
+for (i,nodes) in graph_nodes
+    add_node!(G, Node(string(i), nodes))
 end
 # On ajoute les arêtes au graphe G
 for e in graph_edges
-    add_edge!(G, Edge(string(e[1],"-",e[2]),[Node(string(e[1]), graph_nodes[e[1]]), Node(string(e[2]), graph_nodes[e[2]])], e[3]))
+    add_edge!(G, Edge([Node(string(e[1]), graph_nodes[e[1]]), Node(string(e[2]), graph_nodes[e[2]])], e[3]))
 end
 show(G)
