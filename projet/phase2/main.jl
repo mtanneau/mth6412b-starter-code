6a2e6aafc98406a45f55f614e140f4786c28f5b4
aureliebcx <55095740+aureliebcx@users.noreply.github.com>
2019-10-03 16:25:15 -0400

    Update main.jl
    Suppression des includes en trop

--- projet/phase2/main.jl
+++ projet/phase2/main.jl
@@ -1,10 +1,6 @@
 import Base.show
 
-include(joinpath(@__DIR__,"node.jl"))
-include(joinpath(@__DIR__,"edge.jl"))
-include(joinpath(@__DIR__,"graph.jl"))
 include(joinpath(@__DIR__,"read_stsp.jl"))
-include(joinpath(@__DIR__, "arbreRecouvrement.jl"))
 include(joinpath(@__DIR__, "kruskal.jl"))
 include(joinpath(@__DIR__, "graphConstruction.jl"))
 
