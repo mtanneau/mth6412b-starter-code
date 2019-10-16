2752f85d2bf4a4530f056e4c90bda51f5e66a825
Florence Tanoï Namio <fltana@labos.polymtl.ca>
2019-10-16 17:16:14 -0400

    Modification de runtests.jl

--- projet/phase2/runtests.jl
+++ projet/phase2/runtests.jl
@@ -13,51 +13,36 @@ arete4 = Edge((node1, node4), 4)
 arete5 = Edge((node3, node4), 5)
 G = Graph("Test graph", [node1, node2, node3, node4], [arete1, arete2, arete3, arete4, arete5])
 
-parents=Dict(name(node) => name(node) for node in nodes(G)) #Création d'une forêt d'arborescences avec les noeuds de G, et dont chaque noeud est son propre parent
+parents=Dict(name(node) => name(node) for node in nodes(G)) #Création d'une forêt d'arborescences
+                                                            #avec les noeuds de G, et dont chaque noeud est son propre parent
 
 for node in nodes(G) #Vérification du contenu de parents
     @test parents[name(node)] == name(node)
 end
 
 for edge in edges(G) #On vérifie qu'aucun couple de sommets n'est dans le même ensemble connexe, dans parents
-    @test connex(edge,parents) == false
+    @test !connex(data(edge)[1], data(edge)[2], parents)
 end
 
-union!(arete1,parents) #On ajoute l'arête 1 à parents
-@test parents[name(node2)]==name(node1) #On vérifie que le parent de node2 est bien devenu node1
-@test parents[name(node1)]==name(node1)
-@test connex(arete1,parents) == true #on vérifie le fonctionnement de la fonction connex
-@test connex(arete2,parents) == false
-@test connex(arete3,parents) == false
-@test connex(arete4,parents) == false
-@test connex(arete5,parents) == false
+union_1!(node1, node2, parents) #On ajoute l'arête 1 à parents
+@test connex(node1, node2, parents)  #on vérifie le fonctionnement de la fonction connex
+@test !connex(node2, node4, parents)
+@test !connex(node2, node3, parents)
+@test !connex(node1, node4, parents)
+@test !connex(node3, node4, parents)
 
-union!(arete2,parents)
-@test parents[name(node4)]==name(node2)
-@test parents[name(node2)]==name(node1)
-@test parents[name(node1)]==name(node1)
-@test connex(arete1,parents) == true
-@test connex(arete2,parents) == true
-@test connex(arete3,parents) == false
-@test connex(arete4,parents) == true
-@test connex(arete5,parents) == false
+union_1!(node2, node4,parents)
+@test connex(node1, node2, parents)
+@test connex(node2, node4, parents)
+@test !connex(node2, node3, parents)
+@test connex(node1, node4, parents)
+@test !connex(node3, node4, parents)
 
-#Vérification des racines de chaque noeud, dans le nouveau "parents"
-@test root(node4,parents)==name(node1)
-@test root(node2,parents)==name(node1)
-@test root(node1,parents)==name(node1)
-@test root(node3,parents)==name(node3)
 
-union!(arete3,parents) #Ajout de la dernière arête possible
-
-#Vérification des racines de chaque noeud, dans le nouveau "parents"
-@test root(node4,parents)==name(node1)
-@test root(node2,parents)==name(node1)
-@test root(node1,parents)==name(node1)
-@test root(node3,parents)==name(node1)
+union_1!(node2, node3, parents) #Ajout de la dernière arête possible
 
 for edge in edges(G) #On vérifie que tous les couples de sommets sont dans le même ensemble connexe
-    @test connex(edge,parents) == true
+    @test connex(data(edge)[1], data(edge)[2], parents)
 end
 
 
@@ -68,12 +53,12 @@ typeof(K)==typeof(G)
 total_weight = sum([weight(e) for e in edges(K)])
 
 for e in edges(K)
-@test connex(e,parents)==true #On vérifie que l'ensemble obtenu est connexe
+@test connex(data(e)[1], data(e)[2],parents) #On vérifie que l'ensemble obtenu est connexe
 
 end
 
 @test total_weight==6 #On vérifie que l'arbre obtenu est bien de coût minimal
 
-#On vérifie que K ne contient pas de cycles
+#On vérifie que K ne contient pas les aretes 4 et 5
 @test !(arete4 in edges(K))
 @test !(arete5 in edges(K))
