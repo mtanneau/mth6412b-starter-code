4a1bd94a57a35b88285a4efaeb5f8c28b7b52a6e
Florence Tanoï Namio <fltana@labos.polymtl.ca>
2019-10-16 17:19:26 -0400

    modification de main

--- projet/phase2/main.jl
+++ projet/phase2/main.jl
@@ -7,29 +7,37 @@ include(joinpath(here,"..","phase1","read_stsp.jl"))
 include("kruskal.jl")
 
 filename = joinpath(here,"..","..","instances","stsp","bayg29.tsp")
-header = read_header(filename)
-nom = header["NAME"]
-graph_edges = read_edges(header,filename)
-G = Graph(nom, Node{Vector{Float64}}[], Edge{Vector{Float64}}[])
 
-# On ajoute les noueds un par un au graphe G
-if header["DISPLAY_DATA_TYPE"]=="None"
-    graph_nodes = Dict(i => [NaN,NaN] for i = 1 : parse(Int,header["DIMENSION"]))
-    for (i,nodes) in graph_nodes
-    add_node!(G, Node(string(i), nodes))
-    end
-else
-    graph_nodes = read_nodes(header,filename)
-    for (i,nodes) in graph_nodes
+"""read_graph_stsp(filename)
+
+Construct a graph from given stsp file.
+"""
+function read_graph_stsp(filename::String)
+    header = read_header(filename)
+    nom = header["NAME"]
+    graph_edges = read_edges(header,filename)
+    G = Graph(nom, Node{Vector{Float64}}[], Edge{Vector{Float64}}[])
+
+    # On ajoute les noueds un par un au graphe G
+    if header["DISPLAY_DATA_TYPE"]=="None"
+        graph_nodes = Dict(i => [NaN,NaN] for i = 1 : parse(Int,header["DIMENSION"]))
+        for (i,nodes) in graph_nodes
         add_node!(G, Node(string(i), nodes))
+        end
+    else
+        graph_nodes = read_nodes(header,filename)
+        for (i,nodes) in graph_nodes
+            add_node!(G, Node(string(i), nodes))
+        end
     end
-end
-# On ajoute les arêtes au graphe G
-for e in graph_edges
-    add_edge!(G, Edge((Node(string(e[1]), graph_nodes[e[1]]), Node(string(e[2]), graph_nodes[e[2]])), e[3]))
+    # On ajoute les arêtes au graphe G
+    for e in graph_edges
+        add_edge!(G, Edge((Node(string(e[1]), graph_nodes[e[1]]), Node(string(e[2]), graph_nodes[e[2]])), e[3]))
+    end
+    return G
 end
 
-
+G = read_graph_stsp(filename)
 arbre=kruskal(G)
 show(arbre)
 tree_weight = sum([weight(e) for e in edges(arbre)])
