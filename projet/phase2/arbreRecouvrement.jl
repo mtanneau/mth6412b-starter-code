223f6b1e3e705af5163bb6c11e4f92d2ff5bd010
aureliebcx <55095740+aureliebcx@users.noreply.github.com>
2019-10-15 22:43:45 -0400

    Update arbreRecouvrement.jl

--- projet/phase2/arbreRecouvrement.jl
+++ projet/phase2/arbreRecouvrement.jl
@@ -1,56 +1,50 @@
 import Base.show
+include(joinpath(@__DIR__,"..", "phase1","graph.jl"))
 
-"""Type abstrait dont d'autres types de noeuds dériveront."""
-abstract type AbstractArbre{T} end
 
 """Type représentant les parents de chaque noeud d'un graphe pour un arbre de recouvrement.
+Exemple :
+    node1 = Node(1,2)
+    node2 = Node(2,1)
+    node3 = Node(3,1)
+    edge1 = Edge(node1, node2, 4)
+    edge2 = Edge(node2, node3, 2)
+    edge3 = Edge(node1, node3, 1)
+
+    arbre = ("Arbre de recouvrement", Dict(node1 => node3, node1 => node2, node2 => node3), [edge1, edge2, edge3]
 """
-mutable struct Arbre{T} <: AbstractArbre{T}
+mutable struct Arbre{T} <: AbstractGraph{T}
   name::String
   link::Dict{Node{T}, Node{T}}
   edges::Vector{Edge{T}}
 end
 
-"""Fonction changeant le parent d'un noeud"""
-function changeParent!(tabParents::AbstractArbre, nodeChild::AbstractNode, nodeFather::AbstractNode)
+"""Change le parent d'un noeud"""
+function changeParent!(tabParents::Arbre, nodeChild::AbstractNode, nodeFather::AbstractNode)
   tabParents.link[nodeChild] = nodeFather
   return tabParents
 end
 
-"""Fonction retournant le dictionnaire contenant les noeuds parents de tous les noeuds"""
+"""Retourne le dictionnaire contenant les noeuds parents de tous les noeuds"""
 getParents(graphe::Arbre) = graphe.link
 
-"""Fonction retournant le nom de l'objet arbre"""
-getName(parent::AbstractArbre) = parent.name
 
-"""Fonction récupérant les arêtes"""
-getEdges(arbre::AbstractArbre) = arbre.edges
-
-"""Fonction retournant le parent du noeud donné s'il existe"""
-function getParent(parent::AbstractArbre{T}, noeud::AbstractNode{T}) where T
-  return get(getParents(parent), noeud, 0)
-end
-
-"""Ajoute une arête au graphe."""
-function add_edge!(arbre::AbstractArbre{T}, edge::Edge{T}) where T
-  push!(arbre.edges, edge)
-  arbre
+"""Retourne le parent du noeud donné s'il existe"""
+function getParent(parent::Arbre{T}, noeud::AbstractNode{T}) where T
+  return get(getParents(parent), noeud, ErrorException("le parent n'existe pas"))
 end
 
 
-"""Fonction initialisant un objet de type Arbre pour un graphe"""
+"""Initialise un objet de type Arbre pour un graphe"""
 function initArbre(graphe::AbstractGraph{T}) where T
-  init = Dict{Node{typeNode(graphe)}, Node{typeNode(graphe)}}()
+  init = Dict(node => node for node in nodes(graphe))
   edges = Edge{typeNode(graphe)}[]
   foret = Arbre(name(graphe), init, edges)
-  for noeud in nodes(graphe)
-    foret.link[noeud] = noeud
-  end
   return foret
 end
 
-"""Fonction récupérant la racine du noeud précisé"""
-function getRacine(arbre::AbstractArbre{T}, noeud::AbstractNode{T}) where T
+"""Récupère la racine du noeud précisé"""
+function getRacine(arbre::AbstractGraph{T}, noeud::AbstractNode{T}) where T
   enfant = noeud
   parent = getParent(arbre, noeud)
 
